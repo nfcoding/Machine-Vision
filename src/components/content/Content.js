9c895759ad41072a6de2763adf13acdc302dceb9
nfakhri <nfcoding@gmail.com>
2023-09-30 18:53:39 +0700

    update page not found

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -4,12 +4,14 @@ import { Container } from "reactstrap";
 import { Route, Routes } from "react-router-dom";
 import Post from "../../pages/Post";
 import User from "../../pages/User";
+import NotFound from "../../pages/NotFound";
 
 const Content = ({ sidebarIsOpen, toggleSidebar }) => (
   <Container fluid className={classNames("content", { "is-open": sidebarIsOpen })}>
     <Routes>
       <Route exact path="/" element={<User />} />
       <Route exact path="/post" element={<Post />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   </Container>
 );
